03d642f4ca616653dd73eaa79134aba3df0461fa
Dev <4207793+neroshin@users.noreply.github.com>
2026-04-16 20:34:29 +0800

    Input name
    Signed-off-by: Dev <4207793+neroshin@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
         <div>
           <label>FirstName:</label>
-          <input onChange={(e)=>setFirstName(e.target.value)}/> 
+          <input onChange={(e)=>setFirstName(e.target.value)}/>  
         </div>
 
        <div> {firstName}</div>
